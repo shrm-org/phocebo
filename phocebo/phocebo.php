5110ea00c05b79e2d87c4a781839fa4353d33d27
Patricia Walton <einkinder@mac.com>
2015-10-31 16:36:29 -0400

    Expand phoceboCook - x-auth calculations

--- phocebo/phocebo.php
+++ phocebo/phocebo.php
@@ -57,8 +57,26 @@ if (file_exists(INI)) {
     
 } else die( "\nERROR: Phởcebo ingredients are missing (.env) \n\n");
 
-   
-$params = array();
+
+
+// Test Class
+
+
+$action = '/user/checkUsername';
+
+$data_params = array (
+    
+    'userid' => 'patricia.walton@shrm.org',
+    
+	'also_check_as_email' => true,
+	
+);
+						
+$data = phoceboCook::call($action, $data_params);
+
+var_dump($data);
+
+
 
     
 /**
@@ -73,7 +91,114 @@ $params = array();
  * @package classpackage
  */
  
-class phocebo {
+class phoceboCook {
+
+	/**
+	 * getHash function.
+	 * 
+	 * @access public
+	 * @static
+	 * @data_params mixed $params
+	 * @return array $codice hash value for x_auth
+	 *
+	 */
+	 
+	static public function getHash( $data_params ) {
+    	
+    	if ( !empty ( $data_params ) ) {
+        	
+    		$codice = array( 'sha1' => '', 'x_auth' => '' );
+    		
+    		$codice['sha1'] = sha1 ( implode( ',', $data_params ) . ',' . SECRET );
+    		
+    		$codice['x_auth'] = base64_encode ( KEY . ':' . $codice['sha1'] );
+    		  		
+    		return $codice;
+    		
+    	} else {
+        	
+        	return null;
+        	
+    	}
+		
+	}
+	
+	
+	/**
+	 * getDefaultHeader function.
+	 * 
+	 * @access private
+	 * @static
+	 * @param mixed $x_auth
+	 * @return void
+	 *
+	 */
+	 
+	static private function getDefaultHeader( $x_auth ) {
+    	
+		$host = parse_url ( URL, PHP_URL_HOST );
+		
+ 		return array (
+			
+			"Host: " . ($host ? $host : ''),
+			
+			"Content-Type: multipart/form-data",
+			
+			'X-Authorization: Docebo '.$x_auth,
+			
+		);
+		
+	}
+
+
+	/**
+	 * call function.
+	 * 
+	 * @access public
+	 * @static
+	 * @param mixed $action Docebo API Call
+	 * @param mixed $data_params parameters to send
+	 * @return $output JSON formatted response
+	 *
+	 */
+	 
+	static public function call ( $action, $data_params ) {
+    	
+		$curl = curl_init();
+
+		$hash_info = self::getHash ( $data_params );
+
+		$http_header = self::getDefaultHeader ( $hash_info['x_auth'] );
+
+		$opt = array (
+    		
+			CURLOPT_URL => URL . '/api/' . $action,
+			
+			CURLOPT_RETURNTRANSFER => 1,
+			
+			CURLOPT_HTTPHEADER => $http_header,
+			
+			CURLOPT_POST => 1,
+			
+			CURLOPT_POSTFIELDS => $data_params,
+			
+			CURLOPT_CONNECTTIMEOUT => 5, // Timeout to 5 seconds
+			
+			CURLOPT_SSL_VERIFYPEER => false,
+			
+			CURLOPT_SSL_VERIFYHOST => false,
+			
+		);
+		
+		curl_setopt_array ( $curl, $opt );
+		
+		$output = curl_exec ( $curl );
+		
+		curl_close ( $curl );
+		
+		return $output;
+		
+	}
     
 }
 
