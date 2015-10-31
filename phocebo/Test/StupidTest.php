e5e7b2999a7d7e7a8226ddc4fd92887c94dfe23c
Patricia Walton <einkinder@mac.com>
2015-10-31 09:02:30 -0400

    Initial Phởcebo files - configuration settings and test files

---
+++ phocebo/Test/StupidTest.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * PHPUnit Test Tutorial
+ *
+ * @package PHPUnit
+ * @todo rmeove from repository
+ *
+ */
+
+namespace phocebo\Test;
+
+class StupidTest extends \PHPUnit_Framework_TestCase {
+    
+    public function testTrueIsTrue()     {
+        
+        $foo = true;
+        
+        $this->assertTrue($foo);
+        
+
+        
+    }    
+    
+    
+}
+
+
+?>
