efe375628e4eeef85ca1341ac3e93bf0f4714d3d
Patricia Walton <einkinder@mac.com>
2015-10-31 16:37:02 -0400

    Test case for phoceboCook

---
+++ phocebo/Test/phoceboCookTest.php
@@ -0,0 +1,59 @@
+<?php
+    
+/**
+ * Phởcebo PHPUnit Tests
+ *
+ * Usu fastidii corrumpit honestatis ad, his ludus assueverit id, scripta 
+ * insolens torquatos eu sea. Eum ei maiorum eleifend molestiae, eu mea movet 
+ * placerat iudicabit. Pertinax quaestio te vim, falli utamur senserit in sea, 
+ * vix id magna modus assueverit. No eirmod euismod mel, te his dicta evertitur,
+ * an tota congue consul sed.
+ *
+ * @package Phởcebo
+ * @author Patricia Walton <patricia.walton@shrm.org>
+ * @version 0.0.1 In Development
+ * @license MIT
+ * @copyright 2015 SHRM
+ * 
+ */  
+    
+namespace phocebo\Test;
+
+use phocebo\phoceboCook;
+
+class testphoceboCooking extends \PHPUnit_Framework_TestCase {
+    
+    public function testGetHashParametersExist() {
+        
+        $params = array('userid', 'also_check_as_email');
+        
+        $test = new phoceboCook();
+        
+        $codice = $test->getHash($params);
+        
+        $this->assertNotEmpty($codice, "GetHash returned a Null Value");
+
+    }    
+
+    public function testGetHashsha1String40() {
+        
+        $sha1_len = 0;
+        
+        $params = array('userid', 'also_check_as_email');
+        
+        $test = new phoceboCook();
+        
+        $codice = $test->getHash($params);
+        
+        $sha1_len = strlen ($codice['sha1']);
+        
+        $this->assertEquals(40, $sha1_len, "Sha1 not calculating incorrectly");
+
+    }    
+
+
+    
+}
+
+
+?>
