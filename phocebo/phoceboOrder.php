4696f09137a749bdfbf406d385d57d2db7ada22c
Patricia Walton <einkinder@mac.com>
2015-11-02 07:48:08 -0500

    Possible class for processing calls

---
+++ phocebo/phoceboOrder.php
@@ -0,0 +1,28 @@
+<?php
+    
+/**
+ * Phởcebo User - Delicious PHP wrapper for https://doceboapi.docebosaas.com/api/docs
+ *
+ * Usu fastidii corrumpit honestatis ad, his ludus assueverit id, scripta 
+ * insolens torquatos eu sea. Eum ei maiorum eleifend molestiae, eu mea movet 
+ * placerat iudicabit. Pertinax quaestio te vim, falli utamur senserit in sea, 
+ * vix id magna modus assueverit. No eirmod euismod mel, te his dicta evertitur,
+ * an tota congue consul sed.
+ *
+ * @package Phởcebo User
+ * @author Patricia Walton <patricia.walton@shrm.org>
+ * @version 0.0.1 In Development
+ * @license MIT
+ * @copyright 2015 SHRM
+ *
+ */
+ 
+
+namespace phocebo;
+
+class phoceboOrder extends phoceboCook {
+
+    
+}
+
+?>
