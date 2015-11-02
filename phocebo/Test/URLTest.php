9c330001a06ebe43a2242805abe83e9406a75e74
Patricia Walton <einkinder@mac.com>
2015-11-02 09:31:39 -0500

    Added Tutorial link

--- phocebo/Test/URLTest.php
+++ phocebo/Test/URLTest.php
@@ -34,15 +34,23 @@ class URLTest extends \PHPUnit_Framework_TestCase {
         
     }
     
+    
     public function providerTestSluggifyReturnsSluggifiedString() {
         
         return array(
+            
             array('This string will be sluggified', 'this-string-will-be-sluggified'),
+            
             array('THIS STRING WILL BE SLUGGIFIED', 'this-string-will-be-sluggified'),
+            
             array('This1 string2 will3 be 44 sluggified10', 'this1-string2-will3-be-44-sluggified10'),
+            
             array('This! @string#$ %$will ()be "sluggified', 'this-string-will-be-sluggified'),
+            
             array("Tänk efter nu – förr'n vi föser dig bort", 'tank-efter-nu-forrn-vi-foser-dig-bort'),
+            
             array('', ''),
+            
         );
         
     }        //
