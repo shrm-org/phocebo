29bb070a0d4a00e354c6b31bcae4dbe4fe16b18a
Patricia Walton <einkinder@mac.com>
2015-11-03 18:09:22 -0500

    Begin phoebe class for courses

--- phocebo/phoceboCourse.php
+++ phocebo/phoceboCourse.php
@@ -22,9 +22,292 @@ namespace phocebo;
 
 use phocebo\phoceboCook;
 
-class phoceboUser extends phoceboCook {
+class phoceboCourse extends phoceboCook {
 
+
+    static public function userCourses ( $parameters) {
+        
+/*
+       $action = '/user/checkUsername';
+   
+       $data_params = array (
     
+           'userid'                 => 'patricia.walton@shrm.org',
+    
+           'also_check_as_email'    => true,
+	
+       );
+ 
+       $response = self::call ( $action, $data_params );
+       
+       var_dump($response);
+*/
+
+        
+       if ( !array_key_exists( 'doceboId', $parameters) ) {
+           
+           $json_array = array ('success' => false, 'error' => '301', 'message' => 'Parameter doceboId missing');
+           
+       } else {
+           
+           $action = '/user/userCourses';
+       
+           $data_params = array (
+        
+               'id_user'                 => $parameters['doceboId'],
+    	
+           );
+     
+           $response = self::call ( $action, $data_params );
+           
+           $json_array = json_decode($response, true);
+           
+           if ( false == $json_array['success']) {
+               
+               if ('210' == $json_array['error']) {
+                   
+                   $json_array['message'] = "Invalid User Specification";
+                   
+               }
+    
+               if ('500' == $json_array['error']) {
+                   
+                   $json_array['message'] = 'Internal server error';
+                   
+               }
+    
+           } else { // Success == true
+               
+    
+           }
+           
+       }
+       
+       $responseObj = json_decode ( json_encode( $json_array ), FALSE );
+       
+       return($responseObj);
+ 
+    }
+    
+    
+    static public function listCourses () {
+        
+           $action = '/course/listCourses';
+       
+           $data_params = array (
+        
+               'category'                 => null,
+    	
+           );
+     
+           $response = self::call ( $action, $data_params );
+           
+           $json_array = json_decode($response, true);
+           
+           if ( false == $json_array['success']) {
+    
+               if ('500' == $json_array['error']) {
+                   
+                   $json_array['message'] = 'Internal server error';
+                   
+               }
+    
+           } else { // Success == true
+               
+    
+           }
+        
+       $responseObj = json_decode ( json_encode( $json_array ), FALSE );
+       
+       return($responseObj);
+        
+    }
+
+    static public function listUsersCourses ($parameters) {
+        
+       if ( !array_key_exists( 'doceboId', $parameters) ) {
+           
+           $json_array = array ('success' => false, 'error' => '301', 'message' => 'Parameter doceboId missing');
+           
+       } else {
+
+           $action = '/course/listEnrolledCourses';
+       
+           $data_params = array (
+        
+               'id_user'                 => $parameters['doceboId'],
+    	
+           );
+     
+           $response = self::call ( $action, $data_params );
+           
+           $json_array = json_decode($response, true);
+           
+           if ( false == $json_array['success']) {
+    
+               if ('401' == $json_array['error']) {
+                   
+                   $json_array['message'] = 'Internal server error';
+                   
+               }
+
+               if ('500' == $json_array['error']) {
+                   
+                   $json_array['message'] = 'Internal server error';
+                   
+               }
+    
+           } else { // Success == true
+               
+    
+           }
+           
+       }
+        
+       $responseObj = json_decode ( json_encode( $json_array ), FALSE );
+       
+       return($responseObj);
+        
+    }
+    
+    static public function enrollUserInCourse ($parameters) {
+        
+       if ( !array_key_exists( 'doceboId', $parameters) ) {
+           
+           $json_array = array ('success' => false, 'error' => '301', 'message' => 'Parameter doceboId missing');
+           
+       } else {
+
+           $action = '/course/addUserSubscription';
+       
+           $data_params = array (
+        
+               'id_user'                => $parameters['doceboId'],
+               
+               'course_code'            => $parameters['courseCode'],
+               
+               'user_level'             => 'student'
+    	
+           );
+     
+           $response = self::call ( $action, $data_params );
+           
+           $json_array = json_decode($response, true);
+           
+           if ( false == $json_array['success']) {
+    
+               if ('201' == $json_array['error']) {
+                   
+                   $json_array['message'] = 'Invalid parameters';
+                   
+               }
+
+               if ('202' == $json_array['error']) {
+                   
+                   $json_array['message'] = 'Invalid specified course';
+                   
+               }
+
+               if ('203' == $json_array['error']) {
+                   
+                   $json_array['message'] = 'User already enrolled to the course';
+                   
+               }
+
+               if ('204' == $json_array['error']) {
+                   
+                   $json_array['message'] = 'Error while enrolling user';
+                   
+               }
+
+               if ('500' == $json_array['error']) {
+                   
+                   $json_array['message'] = 'Internal server error';
+                   
+               }
+    
+           } else { // Success == true
+               
+    
+           }
+           
+       }
+        
+       $responseObj = json_decode ( json_encode( $json_array ), FALSE );
+       
+       return($responseObj);
+        
+    }
+
+    static public function unenrollUserInCourse ($parameters) {
+        
+       if ( !array_key_exists( 'doceboId', $parameters) ) {
+           
+           $json_array = array ('success' => false, 'error' => '301', 'message' => 'Parameter doceboId missing');
+           
+       } else {
+
+           $action = '/course/deleteUserSubscription';
+       
+           $data_params = array (
+        
+               'id_user'                => $parameters['doceboId'],
+               
+               'course_code'            => $parameters['courseCode'],
+               
+               'user_level'             => 'student'
+    	
+           );
+     
+           $response = self::call ( $action, $data_params );
+           
+           $json_array = json_decode($response, true);
+           
+           if ( false == $json_array['success']) {
+    
+               if ('201' == $json_array['error']) {
+                   
+                   $json_array['message'] = 'Invalid parameters';
+                   
+               }
+
+               if ('202' == $json_array['error']) {
+                   
+                   $json_array['message'] = 'Invalid specified course';
+                   
+               }
+
+               if ('203' == $json_array['error']) {
+                   
+                   $json_array['message'] = 'User already enrolled to the course';
+                   
+               }
+
+               if ('204' == $json_array['error']) {
+                   
+                   $json_array['message'] = 'Error while enrolling user';
+                   
+               }
+
+               if ('500' == $json_array['error']) {
+                   
+                   $json_array['message'] = 'Internal server error';
+                   
+               }
+    
+           } else { // Success == true
+               
+    
+           }
+           
+       }
+        
+       $responseObj = json_decode ( json_encode( $json_array ), FALSE );
+       
+       return($responseObj);
+        
+    }
+
 }
 
 ?>
