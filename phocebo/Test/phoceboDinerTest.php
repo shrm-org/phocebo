f6ce81c6d22ab45e9e167038ba48b651ce734f9a
Patricia Walton <einkinder@mac.com>
2015-11-03 18:07:52 -0500

    Updated Variables

--- phocebo/Test/phoceboDinerTest.php
+++ phocebo/Test/phoceboDinerTest.php
@@ -24,32 +24,43 @@ use phocebo\phoceboDiner;
 class testphoceboDiner extends \PHPUnit_Framework_TestCase {
     
     /**
-     * testcheckUsername function.
+     * testdoceboId function.
      * 
      * @access public
-     * @param mixed $userId
+     * @param mixed $email
      * @param mixed $expectedResult
      * @param mixed $errorMessage
      * @return void
      *
-     * @dataProvider providerTesttestcheckUsername
+     * @dataProvider providerTesttestdoceboId
+     *
      */
      
-    public function testcheckUsername ($userId, $checkAttribute, $errorMessage ) {
+    public function testdoceboId ( $email, $checkAttribute, $errorMessage ) {
         
-        $responseObj = phoceboDiner::checkUsername( array('userId' => $userId) );
+        $responseObj = phoceboDiner::doceboId( array( 'email' => $email ) );
         
         $this->assertObjectHasAttribute( $checkAttribute, $responseObj, $errorMessage);
 
     }    
     
-    public function providerTesttestcheckUsername() {
+    /**
+     * providerTesttestdoceboId function.
+     *
+     * Test data for the testdoceboId function.
+     
+     * @access public
+     * @return void
+     *
+     */
+     
+    public function providerTesttestdoceboId() {
                
         return array(
             
-            array ('patricia.walton@shrm.org', 'userId', 'UserId is valid but not reporting as valid'),
+            array ( 'patricia.walton@shrm.org', 'email', 'doceboId is valid but not reporting as valid' ),
             
-            array ('someone@example.com', 'error', 'UserId is not valid but reporting as valid'),
+            array ( 'someone@example.com', 'error', 'doceboId is not valid but reporting as valid' ),
             
         );
         
@@ -57,38 +68,36 @@ class testphoceboDiner extends \PHPUnit_Framework_TestCase {
 
    
     /**
-     * testcheckUsernameObj function.
+     * testdoceboIdObj function.
      * 
      * @access public
      * @param mixed $checkAttribute
      * @param mixed $errorMessage
      * @return void
      *
-     * @dataProvider providerTesttestcheckUsernameObj
+     * @dataProvider providerTesttestdoceboIdObj
      */
      
      
-    public function testcheckUsernameObj ($checkAttribute, $errorMessage ) {
+    public function testdoceboIdObj ($checkAttribute, $errorMessage ) {
         
-        $responseObj = phoceboDiner::checkUsername( array('userId' => 'patricia.walton@shrm.org') );
+        $responseObj = phoceboDiner::doceboId( array('email' => 'patricia.walton@shrm.org') );
         
         $this->assertObjectHasAttribute( $checkAttribute, $responseObj, $errorMessage);
 
     }    
     
-    public function providerTesttestcheckUsernameObj() {
+    public function providerTesttestdoceboIdObj() {
                
         return array(
             
-            array ('userId', 'userId not in $responseObj'),
+            array ( 'doceboId', 'doceboId not in $responseObj' ),
 
-            array ('userId', 'userId not in $responseObj'),
-
-            array ('firstname', 'firstname not in $responseObj'),
+            array ( 'firstname', 'firstname not in $responseObj' ),
             
-            array ('lastname', 'lastname not in $responseObj'),
+            array ( 'lastname', 'lastname not in $responseObj' ),
 
-            array ('email', 'email not in $responseObj'),  
+            array ( 'email', 'email not in $responseObj' ),
 
         );
         
@@ -97,41 +106,41 @@ class testphoceboDiner extends \PHPUnit_Framework_TestCase {
     
     
     /**
-     * testcheckUsernameRemovedIdst function.
+     * testdoceboIdRemovedIdst function.
      * 
      * @access public
      * @return void
      *
      */
      
-    public function testcheckUsernameRemovedIdst () {
+    public function testdoceboIdRemovedIdst () {
         
-        $responseObj = phoceboDiner::checkUsername( array('userId' => 'patricia.walton@shrm.org') );
+        $responseObj = phoceboDiner::doceboId ( array( 'email' => 'patricia.walton@shrm.org') );
         
-        $this->assertObjectNotHasAttribute( 'idst', $responseObj, 'The parameter idst should be removed from $responseObj');
+        $this->assertObjectNotHasAttribute ( 'idst', $responseObj, 'The parameter idst should be removed from $responseObj');
 
     }    
     
 
     /**
-     * testcheckUsernameCustomErrors function.
+     * testdoceboIdCustomErrorsJSONformat function.
      * 
      * @access public
      * @return void
      *
-     * @dataProvider providerTesttestcheckUsernameCustomErrorsJSONformat
+     * @dataProvider providerTesttestdoceboIdCustomErrorsJSONformat
      *
      */
      
-    public function testcheckUsernameCustomErrorsJSONformat ($checkAttribute, $errorMessage) {
+    public function testdoceboIdCustomErrorsJSONformat ($checkAttribute, $errorMessage) {
         
-        $responseObj = phoceboDiner::checkUsername( array( 'noUserId' => 'patricia.walton@shrm.org') );
+        $responseObj = phoceboDiner::doceboId( array( 'noemail' => 'patricia.walton@shrm.org') );
         
         $this->assertObjectHasAttribute( $checkAttribute, $responseObj, $errorMessage);
 
     }    
     
-    public function providerTesttestcheckUsernameCustomErrorsJSONformat() {
+    public function providerTesttestdoceboIdCustomErrorsJSONformat() {
                
         return array(
             
@@ -147,36 +156,246 @@ class testphoceboDiner extends \PHPUnit_Framework_TestCase {
 
     
     /**
-     * testcheckUsernameCustomErrors function.
+     * testdoceboIdCustomErrors function.
      * 
      * @access public
      * @param mixed $checkAttribute
      * @param mixed $errorMessage
      * @return void
      *
-     * @dataProvider providerTesttestcheckUsernameCustomErrors
+     * @dataProvider providerTesttestdoceboIdCustomErrors
      *
      */
 
-    public function testcheckUsernameCustomErrors ($parameters, $expected, $errorMessage) {
+    public function testdoceboIdCustomErrors ($parameters, $expected, $errorMessage) {
         
-        $responseObj = phoceboDiner::checkUsername( $parameters );
+        $responseObj = phoceboDiner::doceboId( $parameters );
         
         $this->assertEquals( $responseObj->error, $expected, $errorMessage);
 
     }    
 
-    public function providerTesttestcheckUsernameCustomErrors() {
+    public function providerTesttestdoceboIdCustomErrors() {
                
         return array(
             
-            array ( array ( 'noUserId' => 'patricia.walton@shrm.org'), '301', 'JSON response should be reporting error 301'),
+            array ( array ( 'noemail' => 'patricia.walton@shrm.org' ), '301', 'JSON response should be reporting error 301' ),
 
-            array ( array ( 'userId' => 'not an email address'), '302', 'JSON response should be reporting error 302'),
+            array ( array ( 'email' => 'not an email address' ), '302', 'JSON response should be reporting error 302' ),
 
         );
         
     }
+    
+    
+    // test addUser 
+    // test for required fields
+    // test responses
+    
+    
+    
+    
+    
+    /**
+     * testaddUserCustomErrorsJSONformatfirstName function.
+     * 
+     * @access public
+     * @param mixed $checkAttribute
+     * @param mixed $errorMessage
+     * @return void
+     *
+     * @dataProvider providerTesttestaddUsereCustomErrorsJSONformatfirstName
+     *     
+     */
+     
+     
+    public function testaddUserCustomErrorsJSONformatfirstName ($checkAttribute, $errorMessage) {
+        
+        $parameters = array (
+            
+            'lastName'                  => 'Walton',
+            
+            'email'                     => 'patricia.walton@me.com'
+            
+        );
+        
+        $responseObj = phoceboDiner::addUser( $parameters );
+        
+        $this->assertObjectHasAttribute( $checkAttribute, $responseObj, $errorMessage);
+
+    }    
+    
+    public function providerTesttestaddUsereCustomErrorsJSONformatfirstName() {
+               
+        return array(
+            
+            array ( 'success', 'success not part of JSON response' ),
+
+            array ( 'error', 'error not part of JSON response' ),
+
+            array ( 'message', 'message not part of JSON response' ),
+
+        );
+        
+    }
+
+    
+    /**
+     * testaddUserCustomErrorsJSONformatlastName function.
+     * 
+     * @access public
+     * @param mixed $checkAttribute
+     * @param mixed $errorMessage
+     * @return void
+     *
+     * @dataProvider providerTesttestaddUsereCustomErrorsJSONformatlastName
+     *     
+     */
+     
+     
+    public function testaddUserCustomErrorsJSONformatlastName ($checkAttribute, $errorMessage) {
+        
+        $parameters = array (
+            
+            'firstName'                 => 'Patricia',
+            
+            'email'                     => 'patricia.walton@me.com'
+            
+        );
+        
+        $responseObj = phoceboDiner::addUser( $parameters );
+        
+        $this->assertObjectHasAttribute( $checkAttribute, $responseObj, $errorMessage);
+
+    }    
+    
+    public function providerTesttestaddUsereCustomErrorsJSONformatlastName() {
+               
+        return array(
+            
+            array ( 'success', 'success not part of JSON response' ),
+
+            array ( 'error', 'error not part of JSON response' ),
+
+            array ( 'message', 'message not part of JSON response' ),
+
+        );
+        
+    }
+
+
+    /**
+     * testaddUserCustomErrorsJSONformatemail function.
+     * 
+     * @access public
+     * @param mixed $checkAttribute
+     * @param mixed $errorMessage
+     * @return void
+     *
+     * @dataProvider providerTesttestaddUsereCustomErrorsJSONformatemail
+     *     
+     */
+     
+     
+    public function testaddUserCustomErrorsJSONformatemail ($checkAttribute, $errorMessage) {
+        
+        $parameters = array (
+            
+            'firstName'                 => 'Patricia',
+            
+            'lastName'                  => 'Walton',
+            
+        );
+        
+        $responseObj = phoceboDiner::addUser( $parameters );
+        
+        $this->assertObjectHasAttribute( $checkAttribute, $responseObj, $errorMessage);
+
+    }    
+    
+    public function providerTesttestaddUsereCustomErrorsJSONformatemail() {
+               
+        return array(
+            
+            array ( 'success', 'success not part of JSON response' ),
+
+            array ('error', 'error not part of JSON response' ),
+
+            array ( 'message', 'message not part of JSON response' ),
+
+        );
+        
+    }
+
+
+    /**
+     * testaddUserCustomErrors function.
+     * 
+     * @access public
+     * @param mixed $checkAttribute
+     * @param mixed $errorMessage
+     * @return void
+     *
+     * @dataProvider providerTesttestaddUserCustomErrors
+     *
+     */
+
+    public function testaddUserCustomErrors ($parameters, $expected, $errorMessage) {
+        
+        $responseObj = phoceboDiner::addUser ( $parameters );
+        
+        $this->assertEquals ( $responseObj->error, $expected, $errorMessage );
+
+    }    
+
+    public function providerTesttestaddUserCustomErrors() {
+               
+        return array(
+            
+            array ( array ( 'nofirstName' => 'Patricia' ), '303', 'JSON response should be reporting error 303' ),
+
+            array ( array ( 'firstName' => 'Patricia', 'nolastName' => 'Walton' ), '304', 'JSON response should be reporting error 304' ),
+
+            array ( array ( 'firstName' => 'Patricia', 'lastName' => 'Walton', 'noemail' => 'patrica.walton@me.com' ), '305', 'JSON response should be reporting error 305' ),
+
+        );
+        
+    }
+    
+    
+    
+    
+    public function testaddUser () {
+        
+/*
+object(stdClass)#345 (2) {
+  ["idst"]=>
+  string(5) "12365"
+  ["success"]=>
+  bool(true)
+}        
+*/
+        
+        $parameters = array (
+            
+            'firstName'                 => 'Vladan',
+            
+            'lastName'                  => 'Dasic',
+            
+            'email'                     => 'vdasic@example.com'
+            
+        );
+        
+//         $responseObj = phoceboDiner::addUser( $parameters );
+        
+//         var_dump($responseObj);
+        
+        
+//         $this->assertObjectHasAttribute( $checkAttribute, $responseObj, 'test addUser');
+
+    }    
+    
+    
 
 }
 
